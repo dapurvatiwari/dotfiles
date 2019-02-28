252aa68a4bb7b85cc6fec209f63045ddf3f1e0d2
Apurva Tiwari <d.apurva94@gmail.com>
2019-02-28 12:40:40 +0530

    gnome-terminal-profile added and setupSystem.sh updated to apply changes to terminal

--- setupSystem.sh
+++ setupSystem.sh
@@ -3,3 +3,6 @@ sudo apt-get install vim
 sudo dpkg -i Softwares/mendeleydesktop_1.19.3-stable_amd64.deb 
 sudo dpkg -i Softwares/google-chrome-stable_current_and64.deb 
 sudo bash Softwares/Anaconda3-2018.12-Linux-x86_64.sh
+
+dconf dump /org/gnome/terminal/legacy/profiles:/ > gnome-terminal-profiles.dconf
+dconf load /org/gnome/terminal/legacy/profiles:/ < gnome-terminal-profiles.dconf
