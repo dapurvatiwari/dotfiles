eaa86ce00bb8800e96ab25fffc552b78798c8819
Apurva Tiwari <d.apurva94@gmail.com>
2019-02-28 12:26:32 +0530

    added setupSystem.sh, a shell script to install all basic programs on a formatted, blank PC.

---
+++ setupSystem.sh
@@ -0,0 +1,5 @@
+sudo apt-get install htop
+sudo apt-get install vim
+sudo dpkg -i Softwares/mendeleydesktop_1.19.3-stable_amd64.deb 
+sudo dpkg -i Softwares/google-chrome-stable_current_and64.deb 
+sudo bash Softwares/Anaconda3-2018.12-Linux-x86_64.sh
